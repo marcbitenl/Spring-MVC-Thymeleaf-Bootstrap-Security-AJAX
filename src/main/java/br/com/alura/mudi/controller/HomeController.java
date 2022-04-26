4df1a04ca56927ac18eb0e21d10c14fbf59cb80d
marcb <marcbitenl@gmail.com>
2022-04-26 20:30:11 -0300

    Percorrendo a list na view

--- src/main/java/br/com/alura/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mudi/controller/HomeController.java
@@ -1,13 +1,29 @@
 package br.com.alura.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51Fv-PIiDQL._AC_SL1000_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-4RAM/dp/B07Y9XWK4M/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=823EPLQ86BWM&keywords=xiamio%2Bredmi%2Bnote%2B8&qid=1651014761&sprefix=xiamio%2Bredmi%2Bnote%2B8%2Caps%2C256&sr=8-1&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147&th=1");
+		pedido.setDescricao("uma descricao qualquer paraq esse pedido");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos",pedido);
+		
+		
 		return "home";
 	}
 
