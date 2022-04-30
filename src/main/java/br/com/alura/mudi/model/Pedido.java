76f5f21fd74eba458398026af957df05be66f75c
marcb <marcbitenl@gmail.com>
2022-04-29 22:17:20 -0300

    Usando JPA puro

--- src/main/java/br/com/alura/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mudi/model/Pedido.java
@@ -3,7 +3,16 @@ package br.com.alura.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
