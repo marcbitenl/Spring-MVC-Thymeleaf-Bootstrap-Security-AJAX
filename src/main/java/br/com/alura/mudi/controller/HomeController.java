63baeb5c3315b4f038f8ce86bab5964e1248043a
marcb <marcbitenl@gmail.com>
2022-05-02 20:46:06 -0300

    Injeção de dependencias

--- src/main/java/br/com/alura/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mudi/controller/HomeController.java
@@ -2,28 +2,25 @@ package br.com.alura.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mudi.model.Pedido;
+import br.com.alura.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository repository;
 
 	@GetMapping("/home")
 	public String home(Model model) {
 
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-
+		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
+		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
 
---
+++ src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
@@ -0,0 +1,27 @@
+package br.com.alura.mudi.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mudi.model.Pedido;
+
+@Repository
+public class PedidoRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<Pedido> recuperaTodosOsPedidos() {
+		
+		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		return query.getResultList();
+		
+		
+	}
+
+}
