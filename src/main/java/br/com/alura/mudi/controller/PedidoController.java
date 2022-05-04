12c3192e184f782cd7af3f5fecc6f44b3c6c3eed
marcb <marcbitenl@gmail.com>
2022-05-03 21:46:21 -0300

    Formulario do pedido

---
+++ src/main/java/br/com/alura/mudi/controller/PedidoController.java
@@ -0,0 +1,17 @@
+package br.com.alura.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+	
+	@GetMapping("formulario")
+	public String formulario() {
+		return "pedido/formulario";
+		
+	}
+
+}
