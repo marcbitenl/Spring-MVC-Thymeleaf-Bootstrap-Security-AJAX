14c83c202c953e655c51f8ec693aaa5536342d69
marcb <marcbitenl@gmail.com>
2022-05-09 20:06:33 -0300

    Rotas do status

--- src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mudi/repository/PedidoRepository.java
@@ -1,12 +1,17 @@
 package br.com.alura.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mudi.model.Pedido;
+import br.com.alura.mudi.model.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusPedido status);
 
 
 }
